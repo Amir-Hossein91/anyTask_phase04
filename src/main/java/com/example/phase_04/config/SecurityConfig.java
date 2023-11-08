f552c1e4525b4ef26f5e54914d989bbd32221ac9
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-11-09 03:08:38 +0330

    phase 4: added request Authentication for each controller in SecurityConfig

--- src/main/java/com/example/phase_04/config/SecurityConfig.java
+++ src/main/java/com/example/phase_04/config/SecurityConfig.java
@@ -1,23 +1,48 @@
 package com.example.phase_04.config;
 
+import com.example.phase_04.exceptions.NotFoundException;
+import com.example.phase_04.repository.PersonRepository;
+import com.example.phase_04.service.impl.PersonServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final PersonServiceImpl personService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Bean
-    public SecurityFilterChain  securityFilterChain (HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-
-                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+//                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+                .authorizeHttpRequests(request -> request.requestMatchers("/manager/**").hasRole("MANAGER")
+                .requestMatchers("/customer/**").hasAnyRole("CUSTOMER", "MANAGER")
+                .requestMatchers("/technician/**").hasAnyRole("TECHNICIAN", "MANAGER")
+                .requestMatchers("/person/**").hasAnyRole("CUSTOMER","MANAGER","TECHNICIAN"))
+                .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Autowired
+    @Transactional
+    public void getUser(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(username -> personService.findByUsername(username))
+                .passwordEncoder(passwordEncoder);
     }
 }
