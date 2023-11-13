e3ce8f7769d61a73440d66874a63e09a50675926
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-11-13 19:12:45 +0330

    phase 4: created a mapper for the ManagerOrderReportDTO

--- src/main/java/com/example/phase_04/mapper/OrderMapper.java
+++ src/main/java/com/example/phase_04/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.example.phase_04.mapper;
 
 import com.example.phase_04.dto.request.OrderRequestDTO;
+import com.example.phase_04.dto.response.ManagerOrderReportDTO;
 import com.example.phase_04.dto.response.OrderResponseDTO;
 import com.example.phase_04.entity.Order;
 import org.mapstruct.Mapper;
@@ -33,4 +34,20 @@ public interface OrderMapper {
     @Mapping(target = "address", source = "order.orderDescription.address")
     @Mapping(target = "orderStatus", source = "order.orderStatus")
     OrderResponseDTO modelToDto (Order order);
+
+    @Mapping(target = "subAssistanceTitle", source = "order.subAssistance.title")
+    @Mapping(target = "customerId", source = "order.customer.id")
+    @Mapping(target = "technicianId", source = "order.technician.id")
+    @Mapping(target = "orderRegistrationDateAndTime", source = "order.orderRegistrationDateAndTime")
+    @Mapping(target = "customerSuggestedPrice", source = "order.orderDescription.customerSuggestedPrice")
+    @Mapping(target = "customerDesiredDateAndTime", source = "order.orderDescription.customerDesiredDateAndTime")
+    @Mapping(target = "taskDetails", source = "order.orderDescription.taskDetails")
+    @Mapping(target = "address", source = "order.orderDescription.address")
+    @Mapping(target = "orderStatus", source = "order.orderStatus")
+    @Mapping(target = "acceptedPrice", source = "order.chosenTechnicianSuggestion.techSuggestedPrice")
+    @Mapping(target = "acceptedDate", source = "order.chosenTechnicianSuggestion.techSuggestedDate")
+    @Mapping(target = "estimatedTaskDuration", source = "order.chosenTechnicianSuggestion.taskEstimatedDuration")
+    @Mapping(target = "taskStartDate", source = "order.startedTime")
+    @Mapping(target = "taskFinishDate", source = "order.finishedTime")
+    ManagerOrderReportDTO modelToReport (Order order);
 }
